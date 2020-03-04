1490de507aa7259fd03f15fa6779d9bd06aad22e
yanlele <331393627@qq.com>
2020-03-04 23:04:04 +0800

    feat: add mybatis generator

---
+++ document-01/src/main/java/web/document01/mbg/CommentGenerator.java
@@ -0,0 +1,47 @@
+package web.document01.mbg;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+
+public class CommentGenerator extends DefaultCommentGenerator {
+    private boolean addRemarkComments = false;
+
+    /**
+     * 给用户配置参数
+     */
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    /**
+     * 给字段添加注释
+     */
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        super.addFieldComment(field, introspectedTable, introspectedColumn);
+        String remarks = introspectedColumn.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            addFieldJavaDoc(field, remarks);
+        }
+    }
+
+    private void addFieldJavaDoc(Field field, String remarks) {
+        // 文档注释开始
+        field.addJavaDocLine("/**");
+
+        // 获取数据库字段的备注信息
+        String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+        for (String remarkLine : remarkLines) {
+           field.addJavaDocLine(" * " + remarkLine);
+        }
+        addJavadocTag(field, false);
+        field.addJavaDocLine(" */");
+    }
+}
---
+++ document-01/src/main/java/web/document01/mbg/mapper/PmsBrandMapper.java
@@ -0,0 +1,19 @@
+package web.document01.mbg.mapper;
+
+import web.document01.mbg.model.PmsBrand;
+
+public interface PmsBrandMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PmsBrand record);
+
+    int insertSelective(PmsBrand record);
+
+    PmsBrand selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(PmsBrand record);
+
+    int updateByPrimaryKeyWithBLOBs(PmsBrand record);
+
+    int updateByPrimaryKey(PmsBrand record);
+}
