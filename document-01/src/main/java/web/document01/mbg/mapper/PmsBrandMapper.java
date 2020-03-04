0c37af537ed42e56e153b8d8e334b6ada9647613
yanlele <331393627@qq.com>
2020-03-04 23:36:34 +0800

    feaT: PmsBrandServiceImpl

--- document-01/src/main/java/web/document01/mbg/mapper/PmsBrandMapper.java
+++ document-01/src/main/java/web/document01/mbg/mapper/PmsBrandMapper.java
@@ -1,15 +1,34 @@
 package web.document01.mbg.mapper;
 
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
 import web.document01.mbg.model.PmsBrand;
+import web.document01.mbg.model.PmsBrandExample;
 
+@Component
 public interface PmsBrandMapper {
+    int countByExample(PmsBrandExample example);
+
+    int deleteByExample(PmsBrandExample example);
+
     int deleteByPrimaryKey(Long id);
 
     int insert(PmsBrand record);
 
     int insertSelective(PmsBrand record);
 
+    List<PmsBrand> selectByExampleWithBLOBs(PmsBrandExample example);
+
+    List<PmsBrand> selectByExample(PmsBrandExample example);
+
     PmsBrand selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
+
+    int updateByExample(@Param("record") PmsBrand record, @Param("example") PmsBrandExample example);
 
     int updateByPrimaryKeySelective(PmsBrand record);
 
---
+++ document-01/src/main/java/web/document01/service/PmsBrandService.java
@@ -0,0 +1,19 @@
+package web.document01.service;
+
+import web.document01.mbg.model.PmsBrand;
+
+import java.util.List;
+
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
