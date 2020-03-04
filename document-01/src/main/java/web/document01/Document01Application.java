27185ae38b365962e06ec246b8dfb806e2b99587
yanlele <331393627@qq.com>
2020-03-04 22:09:34 +0800

    feat: add document01

---
+++ document-01/src/main/java/web/document01/Document01Application.java
@@ -0,0 +1,13 @@
+package web.document01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Document01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Document01Application.class, args);
+    }
+
+}
---
+++ document-01/src/test/java/web/document01/Document01ApplicationTests.java
@@ -0,0 +1,13 @@
+package web.document01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Document01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
