bd982dda80e577bb08eb85eab136bff0c30ecdbc
yanlele <331393627@qq.com>
2020-03-08 17:31:37 +0800

    feat: UmsMemberServiceImpl

---
+++ document-demo/src/main/java/web/document/controller/UmsMemberController.java
@@ -0,0 +1,13 @@
+package web.document.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Api(tags = "UmsMemberController", description = "会员登录注册管理")
+@RequestMapping("/sso")
+public class UmsMemberController {
+//    @Autowired
+}
---
+++ document-demo/src/main/java/web/document/service/UmsMemberService.java
@@ -0,0 +1,15 @@
+package web.document.service;
+
+import web.document.common.api.CommonResult;
+
+public interface UmsMemberService {
+    /**
+     * 生成验证码
+     */
+    CommonResult generateAuthCode(String telephone);
+
+    /**
+     * 判断验证码和手机号码是否匹配
+     */
+    CommonResult verifyAuthCode(String telephone, String authCode);
+}
