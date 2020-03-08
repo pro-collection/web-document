91e827835f80da2dbd392b62e28fb43f332728f8
yanlele <331393627@qq.com>
2020-03-08 17:37:29 +0800

    feat: UmsMemberController

--- document-demo/src/main/java/web/document/controller/UmsMemberController.java
+++ document-demo/src/main/java/web/document/controller/UmsMemberController.java
@@ -1,13 +1,32 @@
 package web.document.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import web.document.common.api.CommonResult;
+import web.document.service.UmsMemberService;
 
 @Controller
 @Api(tags = "UmsMemberController", description = "会员登录注册管理")
 @RequestMapping("/sso")
 public class UmsMemberController {
-//    @Autowired
+    @Autowired
+    private UmsMemberService memberService;
+
+    @ApiOperation("获取验证码")
+    @GetMapping("/getAuthCode")
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return memberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation("判断验证码是否正确")
+    @PostMapping("/verifyAuthCode")
+    public CommonResult updatePassword(
+            @RequestParam String telephone,
+            @RequestParam String authCode
+    ) {
+        return memberService.verifyAuthCode(telephone, authCode);
+    }
 }
