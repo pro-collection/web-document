5c7606b5cbb4ec13ce9e2d25fa50d65cf2ce8a97
yanlele <331393627@qq.com>
2020-04-19 22:40:00 +0800

    feat: add demo

--- plus/src/main/java/com/document/plus/PlusApplication.java
+++ plus/src/main/java/com/document/plus/PlusApplication.java
@@ -1,9 +1,11 @@
 package com.document.plus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.document.plus.mapper")
 public class PlusApplication {
 
     public static void main(String[] args) {
--- plus/src/test/java/com/document/plus/PlusApplicationTests.java
+++ plus/src/test/java/com/document/plus/PlusApplicationTests.java
@@ -1,13 +1,29 @@
 package com.document.plus;
 
+import com.document.plus.entry.User;
+import com.document.plus.mapper.UserMapper;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class PlusApplicationTests {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Test
     void contextLoads() {
+        System.out.println(" -------- test ----------");
+        List<User> userList = userMapper.selectList(null);
+        Assert.assertEquals(5, userList.size());
+        userList.forEach(System.out::println);
     }
 
 }
