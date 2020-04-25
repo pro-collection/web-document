25a413a3fe74edc1ad054db244ec64a131aab14a
yanlele <331393627@qq.com>
2020-04-25 20:58:38 +0800

    feat: 添加demo

---
+++ document-run/src/test/java/document/run/RunApplicationTests.java
@@ -0,0 +1,13 @@
+package document.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
