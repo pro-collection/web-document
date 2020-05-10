9889b88fa1aaf6685e35f5212f7adc3a2ea0597a
yanlele <331393627@qq.com>
2020-05-10 14:34:02 +0800

    feat: 添加jwt 工具方法

---
+++ document-demo/docs/05、SpringSecurity和JWT.md
@@ -0,0 +1,23 @@
+## SpringSecurity和JWT 
+
+### 整合SpringSecurity及JWT
+添加依赖
+```xml
+<!--SpringSecurity依赖配置-->
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-security</artifactId>
+</dependency>
+<!--Hutool Java工具包-->
+<dependency>
+    <groupId>cn.hutool</groupId>
+    <artifactId>hutool-all</artifactId>
+    <version>4.5.7</version>
+</dependency>
+<!--JWT(Json Web Token)登录支持-->
+<dependency>
+    <groupId>io.jsonwebtoken</groupId>
+    <artifactId>jjwt</artifactId>
+    <version>0.9.0</version>
+</dependency>
+```
---
+++ document-run/src/main/java/document/run/common/utils/JwtTokenUtil.java
@@ -0,0 +1,26 @@
+package document.run.common.utils;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+/**
+ * JwtToken生成的工具类
+ * JWT token的格式：header.payload.signature
+ * header的格式（算法、token的类型）：
+ * {"alg": "HS512","typ": "JWT"}
+ * payload的格式（用户名、创建时间、生成时间）：
+ * {"sub":"wang","created":1489079981393,"exp":1489684781}
+ * signature的生成算法：
+ * HMACSHA512(base64UrlEncode(header) + "." +base64UrlEncode(payload),secret)
+ */
+public class JwtTokenUtil {
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenUtil.class);
+    private static final String CLAIM_KEY_USERNAME = "sub";
+    private static final String CLAIM_KEY_CREATED = "created";
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private Long expiration;
+}
