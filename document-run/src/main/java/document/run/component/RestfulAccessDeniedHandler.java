2a7d48034ea8daeb378c86e927237083feab6e0c
yanlele <331393627@qq.com>
2020-05-10 20:45:49 +0800

    feat: 当未登录或者token失效访问接口时，自定义的返回结果

--- document-run/src/main/java/document/run/component/RestfulAccessDeniedHandler.java
+++ document-run/src/main/java/document/run/component/RestfulAccessDeniedHandler.java
@@ -4,6 +4,7 @@ import cn.hutool.json.JSONUtil;
 import document.run.common.api.CommonResult;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +14,7 @@ import java.io.IOException;
 /**
  * 访问接口没有权限时，自定义的返回结果
  */
+@Component
 public class RestfulAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
